54346c5034fc51042cbf1844098cbab05a07d1b1
Osvaldo Cruz <osvaldocruzdelacruz2001@gmail.com>
2023-07-11 11:15:11 -0600

    Agregando el sw

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: '.',
+	globPatterns: [
+		'**/*.{html,svg,png,gif,js,json,mp3,css}'
+	],
+	swDest: 'sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
